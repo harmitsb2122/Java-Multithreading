45fae101f9c3bd9042d97e07a7874129b5527f55
harmitsb2122 <harmitsb@gmail.com>
2023-05-23 14:43:26 +0530

    Problems with single thread

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+class MyTask
+{
+    public void run()
+    {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Printer 2 executed task "+i);
+        }
+    }
+}
+
 public class Main {
+
+    // main method represents main thread
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Job 1
+        System.out.println("== Application started ==");
+
+        // Job 2 --> If job2 is a heavy/time consuming task then it may cause app to appear
+        // as it has hung
+        MyTask myTask = new MyTask();
+        myTask.run();
+
+        // Job 3
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Printer 1 executed task "+i);
+        }
+
+        // Job 4
+        System.out.println("== Application terminated ==");
     }
 }
