f615925e83b9eb2a557250e603aaa208d2506db5
harmitsb2122 <harmitsb@gmail.com>
2023-05-23 14:56:21 +0530

    Runnable interface

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-class MyTask extends Thread
+class ParentTask{
+    void print()
+    {
+        System.out.println("Hi there!");
+    }
+}
+class MyTask extends ParentTask implements Runnable
 {
     @Override
     public void run()
@@ -18,8 +24,9 @@ public class Main {
         System.out.println("== Application started ==");
 
         // Job 2 --> Now Job 2 is executed parallely with Job 3
-        MyTask myTask = new MyTask(); // child thread / worker thread
-        myTask.start();
+        Runnable r = new MyTask();
+        Thread t = new Thread(r);
+        t.start();
 
         // Job 3
         for (int i = 1; i <= 10; i++) {
